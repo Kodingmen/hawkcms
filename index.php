7f6bb577a2b322c36687e2e9f35e78b20f1495ff
hoangtu <hoangtu@ohlaalaa.com>
2023-05-05 12:11:23 +0700

    fix duplicate

--- index.php
+++ index.php
@@ -5,10 +5,7 @@ use Illuminate\Http\Request;
 
 require __DIR__.'/vendor/autoload.php';
 
-const ROOT = __DIR__ . DIRECTORY_SEPARATOR;
-const LIBRARIES = ROOT  . "libraries";
-
-$app = $app = require_once __DIR__.'/libraries/bootstrap.php';
+$app = require_once __DIR__.'/libraries/bootstrap.php';
 
 $kernel = $app->make(Kernel::class);
 
