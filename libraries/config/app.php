422a625f8022f38877a3d5e709b8c8ca0b76ea76
hoangtu <hoangtu@ohlaalaa.com>
2023-05-05 15:11:23 +0700

    add Admin

--- libraries/config/app.php
+++ libraries/config/app.php
@@ -168,7 +168,8 @@ return [
 //        // App\Providers\BroadcastServiceProvider::class,
 //        App\Providers\EventServiceProvider::class,
 //        App\Providers\RouteServiceProvider::class,
-         \App\Core\Providers\CoreServiceProvider::class
+         \App\Core\Providers\CoreServiceProvider::class,
+        \App\Admin\Providers\AdminServiceProvider::class
     ])->toArray(),
 
     /*
