18de11619d664957e5b25fcb334d2e0c913b06bd
hoangtu <hoangtu@ohlaalaa.com>
2023-05-05 15:16:03 +0700

    test queue

--- app/Admin/Routes/web.php
+++ app/Admin/Routes/web.php
@@ -1,6 +1,13 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get("/admin", function (){
-    echo "Admin";
+Route::get('/admin', function () {
+    $queue = \Illuminate\Support\Facades\Queue::push('LogMessage',array('message'=>'Time: '.time()));
+    return $queue;
 });
+class LogMessage{
+    public function fire($job, $date){
+        \Illuminate\Support\Facades\File::append(app_path().'/queue.txt',$date['message'].PHP_EOL);
+        $job->delete();
+    }
+}
