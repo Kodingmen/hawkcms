10e06e949a2a3581b066fd2fb9da48918860c63e
hoangtu <hoangtu@ohlaalaa.com>
2023-05-15 11:23:01 +0700

    temp

--- app/Admin/Providers/AdminServiceProvider.php
+++ app/Admin/Providers/AdminServiceProvider.php
@@ -12,9 +12,13 @@ class AdminServiceProvider extends ServiceProvider{
     public function register()
     {
         Route::middleware('web')->group(__DIR__ . '/../Routes/web.php');
+
         $this->loadViewsFrom(__DIR__ . '/../Resources/Views', 'admin');
+
         $this->mergeConfigFrom(__DIR__ . '/../Configs/admin.php', 'admin');
+
         $this->loadMigrationsFrom((dirname(__DIR__) . DIRECTORY_SEPARATOR . "Database/Migrations"));
+
         $this->app->register(EventServiceProvider::class);
     }
 
--- app/Admin/Routes/web.php
+++ app/Admin/Routes/web.php
@@ -2,14 +2,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get("/admin", function (){
-    $city = app(\App\Core\Repository\Cities\CityRepositoryContract::class);
-    dump(\App\Core\Models\City::with('districts')->where('id', '=',2)->get());
-    dump(\App\Core\Models\City::with('districts')->where(function ($q){
-        return $q->where('id', '=',3);
-    })->get());
-    dump($city->with('districts')->where(function ($q){
-        return $q->where('id', '=',2);
-    })->get());
-    dump($city->with('districts')->where('id', '!=',6)->where('id', '=',2)->get());
-    dump($city->with('districts')->where('id', '=',2)->where('id', '!=',6)->get());
+    return view("home");
 });
